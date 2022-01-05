c8bebe8b7cb01618c3a3c3d5064bf8570179fc96
Nhan Tran Viet <nhantran100215@gmail.com>
2022-01-05 10:22:37 +0700

    complete login page

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import "../styles/index.css";
+import LoginPage from "./login/login";
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
